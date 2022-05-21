c1283632865a7d4be1dc96a67766b644994d6638
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-05-21 22:21:09 +0800

    partially integrated welcome page and clean up directory

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+// import App from "./App";
 import LoginForm from "./login-components/LoginForm";
 import RegisterForm from "./login-components/RegisterForm";
 import Account from "./login-components/Account";
+import WelcomePage from "./welcome-pg-components/WelcomePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route path="/" element={<WelcomePage />}></Route>
         <Route path="/register" element={<RegisterForm />}></Route>
         <Route path="/login" element={<LoginForm />}></Route>
         <Route path="/home" element={<Account />}></Route>
---
+++ src/welcome-pg-components/WelcomePage.jsx
@@ -0,0 +1,100 @@
+import styles from "./welcome.css";
+
+function WelcomePage() {
+  return (
+    <body className="background" style={{ styles }}>
+      <div>
+        <img alt="koala" src="./koala.png" className="center" />
+
+        <div class="welcome-text">
+          WELCOME <br />
+          to your <br />
+          Koala Kanban!
+        </div>
+
+        <div class="btn">
+          <a href="index.html">Click to login</a>
+        </div>
+      </div>
+
+      <div class="paw-icon">
+        <div class="circle">
+          <div class="paw">
+            <div class="paw-center"></div>
+            <div class="paw-bottom"></div>
+            <div class="nikukyu1"></div>
+            <div class="nikukyu2"></div>
+            <div class="nikukyu3"></div>
+            <div class="nikukyu4"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="paw-print-1">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+
+      <div class="paw-print-2">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+
+      <div class="paw-print-3">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+
+      <div class="paw-print-4">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+
+      <div class="paw-print-5">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+
+      <div class="paw-print-6">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+
+      <div class="paw-print-7">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+
+      <div class="paw-print-8">
+        <div class="pad large"></div>
+        <div class="pad small-1"></div>
+        <div class="pad small-2"></div>
+        <div class="pad small-3"></div>
+        <div class="pad small-4"></div>
+      </div>
+    </body>
+  );
+}
+
+export default WelcomePage;
