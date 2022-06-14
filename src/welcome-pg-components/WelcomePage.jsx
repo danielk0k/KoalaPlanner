a989a5a624f1552baf581853f07e476427cedf84
Daniel <70556073+danielk0k@users.noreply.github.com>
2022-06-14 15:27:59 +0800

    initial improvement on welcome page

--- src/welcome-pg-components/WelcomePage.jsx
+++ src/welcome-pg-components/WelcomePage.jsx
@@ -1,100 +1,55 @@
-import { Link } from "react-router-dom";
-import styles from "./welcome.css";
+import { useNavigate } from "react-router-dom";
+import {
+  Flex,
+  Spacer,
+  Container,
+  Box,
+  Square,
+  Stack,
+  Button,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 
 function WelcomePage() {
+  const navigate = useNavigate();
   return (
-    <body className="background" style={{ styles }}>
-      <div>
-        <img alt="koala" src="./koala.png" className="center" />
-
-        <div class="welcome-text">
-          WELCOME <br />
-          to your <br />
-          Koala Kanban!
-        </div>
-
-        <div class="btn">
-          <Link to="/app">Click to login</Link>
-        </div>
-      </div>
-
-      <div class="paw-icon">
-        <div class="circle">
-          <div class="paw">
-            <div class="paw-center"></div>
-            <div class="paw-bottom"></div>
-            <div class="nikukyu1"></div>
-            <div class="nikukyu2"></div>
-            <div class="nikukyu3"></div>
-            <div class="nikukyu4"></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="paw-print-1">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-
-      <div class="paw-print-2">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-
-      <div class="paw-print-3">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-
-      <div class="paw-print-4">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-
-      <div class="paw-print-5">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-
-      <div class="paw-print-6">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-
-      <div class="paw-print-7">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-
-      <div class="paw-print-8">
-        <div class="pad large"></div>
-        <div class="pad small-1"></div>
-        <div class="pad small-2"></div>
-        <div class="pad small-3"></div>
-        <div class="pad small-4"></div>
-      </div>
-    </body>
+    <Flex marginTop="5rem" padding="3rem">
+      <Stack spacing={10}>
+        <Square size="60px" bg="#2C3E50" rounded={"lg"}>
+          <Heading textColor="#FFFFFF">K</Heading>
+        </Square>
+        <Stack spacing={8}>
+          <Stack spacing={2}>
+            <Heading size="2xl">Koala Planner</Heading>
+            <Heading size="md" fontWeight="light">
+              Who says Koalas aren't hardworking?
+            </Heading>
+          </Stack>
+          <Text width={{ base: "100%", md: "50%" }}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa modi
+            aspernatur quam est atque id alias accusantium quo voluptates. Error
+            necessitatibus iusto rem velit nesciunt tempora dolores ratione
+            blanditiis harum?
+          </Text>
+          <Button
+            onClick={() => navigate("/app", { replace: true })}
+            textColor="#f8f9fe"
+            bgColor="#34495E"
+            borderWidth="1px"
+            width={{ base: "100%", md: "25%", lg: "15%" }}
+            _hover={{
+              backgroundColor: "#f8f9fe",
+              textColor: "#34495E",
+              borderColor: "#34495E",
+              borderWidth: "1px",
+            }}
+          >
+            Get Started
+          </Button>
+        </Stack>
+      </Stack>
+    </Flex>
   );
 }
 
