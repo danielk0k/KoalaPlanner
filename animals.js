f2a7a3a0531a5fbebf9fa6ff768e3d3470da9f7e
graceyeh111 <97338690+graceyeh111@users.noreply.github.com>
2022-06-16 08:15:18 +0800

    Add files via upload

---
+++ animals.js
@@ -0,0 +1,14 @@
+export const animals = {
+    koala_eat: {
+      image: '/images/koala_eat.png',
+      facts: ['Koalas eat eucalyptus leaves!', 'Koalas eat fruits!', 'Koalas drink water!']
+    },
+    koala_pouch: {
+      image: '/images/koala_pouch.png',
+      facts: ['Koala bears are not bears!', 'Koalas are marzupials!', 'Koalas have a pouch for their young!.']
+    },
+    koala_sleep: {
+      image: '/images/koala_sleep.png',
+      facts: ['Koalas sleep most of the time!', 'Koalas sleep to conserve energy!', 'Koalas sleep high up on trees!']
+    }
+  };
